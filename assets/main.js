4c0b4517e392198cd1c4e42c8062b9c8c2fafb7c
Ben Robertson <benjamingrobertson@me.com>
2017-04-26 12:31:34 -0400

    make conditional

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,11 @@
 const article = document.querySelector('.post-content');
-const h2 = Array.from(article.querySelectorAll('h2'));
-const h3 = Array.from(article.querySelectorAll('h3'));
+if(article) {
+    const h2 = Array.from(article.querySelectorAll('h2'));
+    const h3 = Array.from(article.querySelectorAll('h3'));
 
-h2.forEach((h2) => addlink(h2));
-h3.forEach((h3) => addlink(h3));
+    h2.forEach((h2) => addlink(h2));
+    h3.forEach((h3) => addlink(h3));
+}
 
 function addlink(item) {
     item.insertAdjacentHTML('beforeend', `<a class="heading__anchor-link" href="#${item.id}">#${item.id}</a>`);
