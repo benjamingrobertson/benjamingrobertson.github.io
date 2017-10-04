6a59c749848749aa757d854e19d741412527e796
Ben Robertson <benjamingrobertson@me.com>
2017-10-04 12:55:57 -0400

    Update typography and heading urls

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,10 @@ const article = document.querySelector('.post-content');
 if(article) {
     const h2 = Array.from(article.querySelectorAll('h2'));
     const h3 = Array.from(article.querySelectorAll('h3'));
-
     h2.forEach((h2) => addlink(h2));
     h3.forEach((h3) => addlink(h3));
 }
 
 function addlink(item) {
-    item.insertAdjacentHTML('beforeend', `<a class="heading__anchor-link" href="#${item.id}">#${item.id}</a>`);
+    item.insertAdjacentHTML('beforeend', `<a class="heading__anchor-link" href="#${item.id}">#link</a>`);
 }
